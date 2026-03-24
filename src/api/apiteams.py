ad0787d791ec255fe1ae5daa7445ee397f1d0988
Deni Bisultanov <turboflashgg@gmail.com>
2026-03-24 17:12:19 +0500

    Сделал ручку для получения игрока, добавил флаги

--- src/api/apiteams.py
+++ src/api/apiteams.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , HTTPException
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 from src.database.database import SessionDep
 from src.database.models.teams import Team
 from src.database.models.players import Player
 
 router = APIRouter()
+
+# TEAMS
+# TEAMS
 
 @router.get('/teams')
 async def get_teams(ses: SessionDep):
@@ -18,5 +22,48 @@ async def get_teams(ses: SessionDep):
          'logo_url' : team.logo_url}
          for team in teams
     ]
+
+@router.get('/teams/{team_slug}')
+async def get_team(team_slug , ses: SessionDep):
+    result = await ses.execute(select(Team).where(Team.slug == team_slug))
+    team = result.scalar_one_or_none()
+
+    if not team:
+        raise HTTPException(status_code=404, detail='Team not found!')
+
+    return {
+        'id' : team.id,
+        'name' : team.name,
+        'slug' : team.slug,
+        'country_code' : team.country_code,
+        'flag_url' : f'/static/photos/{team.country_code.lower()}.svg',
+        'region' : team.region,
+        'logo_url' : team.logo_url,
+        'description' : team.description
+    }
+
+@router.get('/players/{nickname}')
+async def get_player_in_window(nickname , ses: SessionDep):
+    result = await ses.execute(select(Player).options(selectinload(Player.team)).where(Player.nickname == nickname))
+    player = result.scalar_one_or_none()
+
+    if not player:
+        raise HTTPException(status_code=404, detail='Player not found')
+    
+    return {
+        "nickname": player.nickname,
+        "real_name": player.real_name,
+        "country_code": player.country_code,
+        "flag_url" : f'/static/flags/{player.country_code.lower()}.svg',
+        "role": player.role,
+        "photo_url": player.photo_url,
+        "team" : {
+            "name" : player.team.name,
+            "slug" : player.team.slug,
+            "region" : player.team.region
+        }
+    }
+
+    
 
 
