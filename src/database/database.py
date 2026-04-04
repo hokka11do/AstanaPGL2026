1c68d24f8610c9e22e6a26279df1afc7b238fd29
Deni Bisultanov <turboflashgg@gmail.com>
2026-04-04 12:14:24 +0500

    Изменил немного структуру для более удобного CD

--- src/database/database.py
+++ src/database/database.py
@@ -1,26 +1,19 @@
 from sqlalchemy.orm import DeclarativeBase , mapped_column , Mapped
 from sqlalchemy.ext.asyncio import create_async_engine , async_sessionmaker , AsyncSession
-from dotenv import load_dotenv
-import os
 from pydantic_settings import BaseSettings , SettingsConfigDict
 from fastapi import Depends
 from typing import Annotated
 
 
 class Settings(BaseSettings):
-    DB_USER : str
-    DB_PASSWORD : str
-    DB_HOST : str
-    DB_PORT : int
-    DB_NAME : str
+    DATABASE_URL : str
 
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
 settings = Settings()
 
 
-engine = create_async_engine(f'postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}'
-                             f'@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}')
+engine = create_async_engine(settings.DATABASE_URL)
 
 session = async_sessionmaker(
     engine,
